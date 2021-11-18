f7eb29a6c80151079da771c93a03137369027189
Seyiul <63714074+Seyiul@users.noreply.github.com>
2021-11-18 14:52:13 +0900

    Create 64065.py

---
+++ programmers/Lv.2/64065.py
@@ -0,0 +1,25 @@
+def solution(s):
+    nums = []
+    s = list(s[2:-2].split('},{'))
+
+    for i in s:
+        i = list(map(int,i.split(',')))
+        nums.append(i)
+
+    dic = {num : 0 for num in max(nums,key=len)}
+    print(dic)
+
+    for i in nums:
+        for d in dic:
+            if d in i:
+                dic[d] += 1
+    print(dic)
+
+    sorted_dic = sorted(dic.items(), reverse =True, key=lambda item:item[1])
+
+    answer = []
+
+    for key, value in sorted_dic:
+         answer.append(key)
+
+    return answer

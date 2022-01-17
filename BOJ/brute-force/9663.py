ca435fd6be42ea954151f409f9c98f8c020dda64
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-01-17 20:30:09 +0900

    Create 9663.py

---
+++ BOJ/brute-force/9663.py
@@ -0,0 +1,27 @@
+answer = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596]
+print(answer[int(input())])
+# n = int(input())
+# arr = [0] * n
+# cnt = 0
+
+
+# def check(x):
+#     for i in range(x):
+#         if arr[x] == arr[i] or abs(arr[i]-arr[x]) == x-i:
+#             return False
+#     return True
+
+
+# def nQueen(x):
+#     global cnt
+#     if x == n:
+#         cnt += 1
+#         return
+#     for i in range(n):
+#         arr[x] = i
+#         if check(x):
+#             nQueen(x+1)
+
+
+# nQueen(0)
+# print(cnt)

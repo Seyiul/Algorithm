93b42b2184227a2bd439afd1f1aa0b5682ac13c2
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-03-06 01:27:11 +0900

    Sort

---
+++ programmers/Lv.2/42842.py
@@ -0,0 +1,11 @@
+def solution(brown, yellow):
+    answer = []
+    yellow_x, yellow_y = 0, 0
+    for i in range(1, yellow+1):
+        if yellow % i == 0:
+            yellow_x = yellow//i
+            yellow_y = i
+            if(yellow_x * 2) + (yellow_y * 2) + 4 == brown:
+                answer = [yellow_x+2, yellow_y + 2]
+                break
+    return answer
